efe253582a41201b2138436955a952b9b9cd6ec1
thuan0311 <bachthuan03111997@gmail.com>
2019-06-02 04:07:14 +0700

    Update GP.py

--- Source_code/GP.py
+++ Source_code/GP.py
@@ -5,7 +5,6 @@
 #                   - convertFullBluetree_to_oriBluetree()
 #                   - get_val_frombluetree()
 #                   - addsub3()
-# linh.homanh@hust.edu.vn - 2018
 # bachthuan03111997@gmail.com - 2018
 import random
 import geopandas as gpd
